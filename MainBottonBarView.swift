63392fea344635b3c21eaf2462499e361a54a3e5
Aitor Sola <solasola1206@hotmail.com>
2021-07-12 11:33:54 +0200

    minor fixes and updates

--- MainBottonBarView.swift
+++ MainBottonBarView.swift
@@ -9,105 +9,88 @@ import UIKit
 import Reusable
 import SnapKit
 
-public enum MainBarOption {
-    case discover
-    case build
-    case profile
-}
-
 public struct BottomBarItem {
-    var image: UIImage
-    var onTap: (() -> Void)?
-    
-    public init(image: UIImage, onTap: (() -> Void)?) {
-        self.image = image
-        self.onTap = onTap
-        
-    }
-}
-
-public enum BottomBarAnimation {
-    case jump
-}
-
-public protocol MainButtonBarViewDelegate: class {
-    func optionDidChange(_ option: MainBarOption)
+  var image: UIImage
+  var onTap: (() -> Void)?
+  
+  public init(image: UIImage, onTap: (() -> Void)?) {
+    self.image = image
+    self.onTap = onTap
+  }
 }
 
 public class MainBottonBarView: UIView, NibOwnerLoadable {
-    
-    let impact = UIImpactFeedbackGenerator(style: .medium)
-    var shouldHaptic: Bool = true
-    
-    @IBOutlet weak var otMainIconStackView: UIStackView!
-    @IBOutlet weak var otView: UIView!
- 
-    weak var delegate: MainButtonBarViewDelegate?
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        loadNibContent()
-        configureView()
-    }
-    
-    public func setItems(_ items: [BottomBarItem]) {
-        otMainIconStackView.resetAllViews()
-        for item in items {
-            let view = UIView()
-            let button = UIButton()
-            let imageView = UIImageView()
-            imageView.contentMode = .scaleAspectFit
-            imageView.image = item.image
-            view.addSubview(imageView)
-            view.addSubview(button)
-            imageView.bindFrameToSuperviewBounds()
-            button.bindFrameToSuperviewBounds()
-            otMainIconStackView.addArrangedSubview(view)
-            button.addAction { [weak self] in
-                self?.animateWhenTouched(view)
-                if self?.shouldHaptic ?? true {
-                    self?.impact.impactOccurred()
-                }
-                item.onTap?()
-            }
+  
+  let impact = UIImpactFeedbackGenerator(style: .medium)
+  var shouldHaptic: Bool = true
+  
+  @IBOutlet weak var mainIconStackView: UIStackView!
+  @IBOutlet weak var mainView: UIView!
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    loadNibContent()
+    configureView()
+  }
+  
+  public func setItems(_ items: [BottomBarItem]) {
+    mainIconStackView.resetAllViews()
+    items.forEach({ item in
+      let view = UIView()
+      let button = UIButton()
+      let imageView = UIImageView()
+      imageView.contentMode = .scaleAspectFit
+      imageView.image = item.image
+      view.addSubview(imageView)
+      view.addSubview(button)
+      imageView.bindFrameToSuperviewBounds()
+      button.bindFrameToSuperviewBounds()
+      mainIconStackView.addArrangedSubview(view)
+      button.addAction { [weak self] in
+        self?.animateWhenTapped(view)
+        if self?.shouldHaptic ?? true {
+          self?.impact.impactOccurred()
         }
+        item.onTap?()
+      }
+    })
+  }
+  
+  private func animateWhenTapped(_ item: UIView) {
+    UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
+      item.transform = CGAffineTransform.identity.scaledBy(x: 0.9, y: 0.9)
+    }) { _ in
+      UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
+        item.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 1)
+      })
     }
-    
-    private func animateWhenTouched(_ item: UIView) {
-        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
-            item.transform = CGAffineTransform.identity.scaledBy(x: 0.9, y: 0.9)
-        }) { (_) in
-            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
-                item.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 1)
-            })
-        }
+  }
+  
+  private func configureView() {
+    mainView.backgroundColor = .clear
+    layer.masksToBounds = true
+    layer.cornerRadius = frame.height/2
+  }
+  
+  public func startAnimation() {
+    self.snp.updateConstraints { maker in
+      maker.width.equalToSuperview().multipliedBy(0.85)
     }
-    
-    private func configureView() {
-        otView.backgroundColor = .clear
-        layer.masksToBounds = true
-        layer.cornerRadius = frame.height/2
-    }
-    
-    public func startAnimation() {
-        self.snp.updateConstraints { maker in
-            maker.width.equalToSuperview().multipliedBy(0.85)
-        }
-        UIView.animate(withDuration: 0.3, animations: { [weak self] in
-            self?.layoutIfNeeded()
-        })
-    }
+    UIView.animate(withDuration: 0.3, animations: { [weak self] in
+      self?.layoutIfNeeded()
+    })
+  }
 }
 
 public class ClosureSleeve {
-    let closure: () -> ()
-    
-    public init(attachTo: AnyObject, closure: @escaping () -> ()) {
-        self.closure = closure
-        objc_setAssociatedObject(attachTo, "[\(arc4random())]", self, .OBJC_ASSOCIATION_RETAIN)
-    }
-    
-    @objc public func invoke() {
-        closure()
-    }
+  let closure: () -> ()
+  
+  public init(attachTo: AnyObject, closure: @escaping () -> ()) {
+    self.closure = closure
+    objc_setAssociatedObject(attachTo, "[\(arc4random())]", self, .OBJC_ASSOCIATION_RETAIN)
+  }
+  
+  @objc public func invoke() {
+    closure()
+  }
 }
--- extensions/Extensions.swift
+++ extensions/Extensions.swift
@@ -9,36 +9,36 @@
 import UIKit
 
 public extension UIStackView {
-    func resetAllViews() {
-        let removedSubviews = self.arrangedSubviews.reduce([]) { (allSubviews, subview) -> [UIView] in
-            self.removeArrangedSubview(subview)
-            return allSubviews + [subview]
-        }
-        
-        // Deactivate all constraints
-        NSLayoutConstraint.deactivate(removedSubviews.flatMap({ $0.constraints }))
-        
-        // Remove the views from self
-        removedSubviews.forEach({ $0.removeFromSuperview() })
+  func resetAllViews() {
+    let removedSubviews = self.arrangedSubviews.reduce([]) { (allSubviews, subview) -> [UIView] in
+      self.removeArrangedSubview(subview)
+      return allSubviews + [subview]
     }
+    
+    // Deactivate all constraints
+    NSLayoutConstraint.deactivate(removedSubviews.flatMap({ $0.constraints }))
+    
+    // Remove the views from self
+    removedSubviews.forEach({ $0.removeFromSuperview() })
+  }
 }
 
 public extension UIView {
-    func bindFrameToSuperviewBounds() {
-        guard let superview = self.superview else {
-            print("Error! `superview` was nil – call `addSubview(view: UIView)` before calling `bindFrameToSuperviewBounds()` to fix this.")
-            return
-        }
-        
-        self.translatesAutoresizingMaskIntoConstraints = false
-        superview.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[subview]-0-|", options: .directionLeadingToTrailing, metrics: nil, views: ["subview": self]))
-        superview.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[subview]-0-|", options: .directionLeadingToTrailing, metrics: nil, views: ["subview": self]))
+  func bindFrameToSuperviewBounds() {
+    guard let superview = self.superview else {
+      print("Error! `superview` was nil – call `addSubview(view: UIView)` before calling `bindFrameToSuperviewBounds()` to fix this.")
+      return
     }
+    
+    self.translatesAutoresizingMaskIntoConstraints = false
+    superview.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[subview]-0-|", options: .directionLeadingToTrailing, metrics: nil, views: ["subview": self]))
+    superview.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[subview]-0-|", options: .directionLeadingToTrailing, metrics: nil, views: ["subview": self]))
+  }
 }
 
 public extension UIControl {
-    func addAction(for controlEvents: UIControl.Event = .touchUpInside, action: @escaping () -> ()) {
-        let sleeve = ClosureSleeve(attachTo: self, closure: action)
-        addTarget(sleeve, action: #selector(ClosureSleeve.invoke), for: controlEvents)
-    }
+  func addAction(for controlEvents: UIControl.Event = .touchUpInside, action: @escaping () -> ()) {
+    let sleeve = ClosureSleeve(attachTo: self, closure: action)
+    addTarget(sleeve, action: #selector(ClosureSleeve.invoke), for: controlEvents)
+  }
 }
